e8f906dae542e94a5442047a0ef58fd98f46c54a
Serhii Bihun <s.bihun93@gmail.com>
2024-02-17 15:28:05 +0200

    Create templates for exception tests.

--- BrainfuckInterpreter.UnitTest/BrainfuckOptimizedInterpreterTest.cs
+++ BrainfuckInterpreter.UnitTest/BrainfuckOptimizedInterpreterTest.cs
@@ -83,4 +83,107 @@ public class BrainfuckOptimizedInterpreterTest
         memoryStream.Position = 0;
         Assert.AreEqual("0123456789", new StreamReader(memoryStream).ReadToEnd());
     }
+
+    [TestMethod]
+    public void AccessingNegativeIndexMemoryCells()
+    {
+        
+        var program = "<>";
+        var intpuStream = new StreamReader(new MemoryStream());
+        var memoryStream = new MemoryStream();
+        var outputStream = new StreamWriter(memoryStream);
+        Interpreter?.Interpret(program, intpuStream, outputStream); // should throw out of memory
+    }
+
+    [TestMethod]
+    public void RequestingTooManyMemory()
+    {
+        var program = $";{{STOP_DEBUGGING}}+[>+]";
+        var intpuStream = new StreamReader(new MemoryStream());
+        var memoryStream = new MemoryStream();
+        var outputStream = new StreamWriter(memoryStream);
+        Interpreter?.Interpret(program, intpuStream, outputStream); // out of memory
+    }
+
+    [TestMethod]
+    public void ProgramIsTooLong()
+    {
+        var program = Enumerable.Repeat('+', 30001).ToString();
+        var intpuStream = new StreamReader(new MemoryStream());
+        var memoryStream = new MemoryStream();
+        var outputStream = new StreamWriter(memoryStream);
+        Interpreter?.Interpret(program, intpuStream, outputStream); // program is too long
+    }
+
+    [TestMethod]
+    public void LoopHasNoEnd()
+    {
+        var program = "[";
+        var intpuStream = new StreamReader(new MemoryStream());
+        var memoryStream = new MemoryStream();
+        var outputStream = new StreamWriter(memoryStream);
+        Interpreter?.Interpret(program, intpuStream, outputStream); // loop has no end
+    }
+
+    [TestMethod]
+    public void LoopHasNoStart()
+    {
+        var program = "]";
+        var intpuStream = new StreamReader(new MemoryStream());
+        var memoryStream = new MemoryStream();
+        var outputStream = new StreamWriter(memoryStream);
+        Interpreter?.Interpret(program, intpuStream, outputStream); // loop has no start
+    }
+
+    [TestMethod]
+    public void InputIsNull()
+    {
+        var program = ",";
+        var intpuStream = (StreamReader)null;
+        var memoryStream = new MemoryStream();
+        var outputStream = new StreamWriter(memoryStream);
+        Interpreter?.Interpret(program, intpuStream, outputStream); // null pointer
+    }
+
+    [TestMethod]
+    public void InputStreamIsInaccessible()
+    {
+        var program = ",";
+        var intpuStream = new StreamReader(new MemoryStream());
+        var memoryStream = new MemoryStream();
+        var outputStream = new StreamWriter(memoryStream);
+        Interpreter?.Interpret(program, intpuStream, outputStream); // meaningful exception
+    }
+    
+    [TestMethod]
+    public void OutputIsNull()
+    {
+        var program = ".";
+        var intpuStream = new StreamReader(new MemoryStream());
+        var memoryStream = new MemoryStream();
+        var outputStream = (StreamWriter)null;
+        Interpreter?.Interpret(program, intpuStream, outputStream); // null pointer
+    }
+
+    [TestMethod]
+    public void OutputStreamIsInaccessible()
+    {
+        var program = ".";
+        var intpuStream = new StreamReader(new MemoryStream());
+        var memoryStream = new MemoryStream();
+        var outputStream = new StreamWriter(memoryStream);
+        Interpreter?.Interpret(program, intpuStream, outputStream); // meaningful exception
+    }
+
+    [TestMethod]
+    public void UnexpectedExceptionShouldBeRethrown()
+    {
+        var program = "+";
+        var intpuStream = new StreamReader(new MemoryStream());
+        var memoryStream = new MemoryStream();
+        var outputStream = new StreamWriter(memoryStream);
+        Interpreter?.Interpret(program, intpuStream, outputStream); // exception should be rethrown
+    }
+
+    // TODO: anything else in terms of exceptions?
 }
